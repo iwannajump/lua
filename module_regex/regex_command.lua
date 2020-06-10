dfb6fad668e8f5833f670d474f2baf542d19a89b
iwannajump <59066701+iwannajump@users.noreply.github.com>
2020-06-10 15:13:17 +0200

    Update regex_command.lua

--- module_regex/regex_command.lua
+++ module_regex/regex_command.lua
@@ -1,10 +1,10 @@
 function command_regex( account, message )
 	local regex_match = message:match "^regex%s(.*)$"
 	if regex_match then
-		local regex = 	regex_match:match "%((.*)%)%s"
-			  regex = 	string.gsub(regex, "\\", "%%")
+		local regex 	= regex_match:match "%((.*)%)%s"
+		regex 		= string.gsub(regex, "\\", "%%")
 		local text 	= 	regex_match:match "%)%s.*$"
-			  text  = 	text:gsub ("%)", "")
+		text  		= text:gsub ("%)", "")
 		send_message(account, text:match(regex))
 	end
 end
