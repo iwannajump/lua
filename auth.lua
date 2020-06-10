d0d811ab29fb57a2b4e411cafc5bd6a32ff87ddf
iwannajump <59066701+iwannajump@users.noreply.github.com>
2020-06-10 15:11:41 +0200

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -1,5 +1,5 @@
-account 				    = { access_token, api_version, user_id, user_token }
-account.user_id				  = ""
-account.user_token          = "" 	-- user
-account.access_token	        = ""	-- group
-account.api_version			 = "5.52"
+account= { access_token, api_version, user_id, user_token }
+account.user_id   = ""
+account.user_token  = "" 	-- user
+account.access_token= ""	-- group
+account.api_version= "5.52"
