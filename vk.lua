fe715da753620d0326afe66255d5bec909b3c4da
iwannajump <59066701+iwannajump@users.noreply.github.com>
2020-06-10 19:25:25 +0200

    Update vk.lua

--- vk.lua
+++ vk.lua
@@ -1,45 +1,49 @@
-local vk 						= require "lib"
-local admin_lib 				= require "module_admin/lib_admin"
+local vk 				= require "lib"
+local admin_lib 			= require "module_admin/lib_admin"
 local admin_commands 			= require "module_admin/admin_commands"
-local math_lib 					= require "module_math/lib_math"
+local math_lib 				= require "module_math/lib_math"
 local math_commands 			= require "module_math/math_commands"
 local regex_command 			= require "module_regex/regex_command"
 local vk_api_commands 			= require "module_handling_api/vk_api_commands"
-local auth 						= require "auth"				--init account variable
+local auth 				= require "auth"				--init `account` variable
 
 while true do
 
-	local long_poll				= call(account, "groups.getLongPollServer", { group_id = "192764727" })
-	local lp_server 			= long_poll["response"]["server"]
-	local lp_key 				= long_poll["response"]["key"]
-	local lp_ts 				= long_poll["response"]["ts"]
+	local long_poll			= call(account, "groups.getLongPollServer", { group_id = "192764727" })
+	local lp_server 		= long_poll["response"]["server"]
+	local lp_key 			= long_poll["response"]["key"]
+	local lp_ts 			= long_poll["response"]["ts"]
 
-	local answer = 				request(lp_server,
-								{act = "a_check", key = lp_key,
-								ts = lp_ts, wait = "30"})
+	local answer = 			request(lp_server,
+					{act = "a_check", key = lp_key,
+					ts = lp_ts, wait = "30"})
 
 	if answer_not_empty(answer) == true then
 
-		message 				= answer["updates"][1]["object"]["message"]["text"]
-		from_id 				= answer["updates"][1]["object"]["message"]["from_id"]
-		account.peer 			= answer["updates"][1]["object"]["message"]["peer_id"]
+		message 		= answer["updates"][1]["object"]["message"]["text"]
+		from_id 		= answer["updates"][1]["object"]["message"]["from_id"]
+		account.peer 		= answer["updates"][1]["object"]["message"]["peer_id"]
 			
 		if message ==  "помощь" then
 	    send_message( account, 	"Бот целиком и полностью написанный на Lua" .. "\n" .. 
-								"Список команд: " .. "\n" .. 
-								"1 ... видео {название}" .. "\n" .. 
-								"2 ... фибоначчи | fibonacci {число}" .. "\n" .. 
-								"3 ... факториал | factorial {число}" .. "\n" .. 
-								"4 ... rgb to hex {0...255, 0...255, 0...255}" .. "\n" .. 
-								"5 ... hex to rgb {#шестизначное hex-число}" .. "\n" ..
-								"6 ... calc {выражение}" .. "\n" .. 
-								"7 ... equ {квадратный трёхчлен} (для тупеньких, ax^2+bx+c)" .. "\n" ..
-								"8 ... regex {(регулярное выражение)} {текст}")
+					"Список команд: " .. "\n" .. 
+					"1 ... видео {название}" .. "\n" .. 
+					"2 ... фибоначчи | fibonacci {число}" .. "\n" .. 
+					"3 ... факториал | factorial {число}" .. "\n" .. 
+					"4 ... rgb to hex {0...255, 0...255, 0...255}" .. "\n" .. 
+					"5 ... hex to rgb {#шестизначное hex-число}" .. "\n" ..
+					"6 ... calc {выражение}" .. "\n" .. 
+					"7 ... equ {квадратный трёхчлен}" .. "\n" ..
+					"8 ... regex {(регулярное выражение)} {текст}")
 		end
 
-		if message then
+		if message == "тест" then
+		send_message ( account, "ок") end
 
-			command_video_search( account, user_token, message )
+		if message then
+			print(message)
+
+			command_video_search( user_token, message )
 
 			command_fibonacci 	( account, message )
 
@@ -57,12 +61,12 @@ while true do
 
 			if from_id == account.user_id then
 
-				bash ( account, message )
+				bash ( message )
 
-				lua_exec ( account, message )
+				lua_exec ( message )
 
 			else
-				error_403_message ( account, message )
+				error_403_message ( message )
 			end
 		end
 	end
