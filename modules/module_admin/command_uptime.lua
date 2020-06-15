20baa03ca44655853db86375acaec9728f9ef851
oxfffffe <59066701+oxfffffe@users.noreply.github.com>
2020-06-15 21:58:41 +0200

    Update command_uptime.lua

--- modules/module_admin/command_uptime.lua
+++ modules/module_admin/command_uptime.lua
@@ -1,3 +1,5 @@
 function command_uptime ( message, start_time )
-	send_message( account, "Бот был запущен в " .. start_time)
+	if message == "?аптайм" then
+		send_message( account, "Бот был запущен в " .. start_time)
+	end
 end
